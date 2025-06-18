330394865fee70ad40e8f165a73fbe01583b751e
Omrani Doua <omrani.doua@gmail.com>
2025-06-18 13:02:18 +0100

    ✅ fix: lowercase filenames for Render compatibility

--- backend/routes/partner.js
+++ backend/routes/partner.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-const Partner = require("../models/Partner");
+const Partner = require("../models/partner");
 
 // ✅ Inscription partenaire
 router.post("/register", async (req, res) => {

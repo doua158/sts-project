7fdfe48daafcdbb086b12ad398efefc2a45ccff8
Omrani Doua <omrani.doua@gmail.com>
2025-06-21 13:16:09 +0100

    Projet React prêt  déployer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import DashboardAdmin from "./pages/AdminDashboard";
-import DashboardPartenaire from "./pages/Dashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />             {/* ✅ Page d'accueil */}
+        <Route path="/login" element={<Login />} />       {/* ✅ Page de connexion */}
         <Route path="/dashboard-admin" element={<DashboardAdmin />} />
-        <Route path="/dashboard-partenaire" element={<DashboardPartenaire />} />
       </Routes>
     </Router>
   );
